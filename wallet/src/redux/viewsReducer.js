c0cd00676e9ae08e597bc870b881089dc666f8b0
Aditya Bose <adi26mar@gmail.com>
2020-07-19 15:53:08 +0530

    Add CurrencySelector component

--- wallet/src/redux/viewsReducer.js
+++ wallet/src/redux/viewsReducer.js
@@ -1,5 +1,11 @@
 import VIEWS from '../views'
 
+const previousIndexes = {
+  [VIEWS.WALLET_UNLOCK]: -1,
+  [VIEWS.CURRENCY_SELECT]: VIEWS.WALLET_UNLOCK,
+  [VIEWS.WALLET_CREATE]: VIEWS.CURRENCY_SELECT,
+}
+
 const defaultState = {
   index: VIEWS.WALLET_UNLOCK,
   previousIndex: -1,
@@ -8,10 +14,13 @@ const defaultState = {
 export default (state = defaultState, action) => {
   switch (action.type) {
     case 'VIEWS.SET_VIEW': {
+      const previousIndex = previousIndexes[action.data]
+
       return {
         ...state,
         index: action.data,
-        previousIndex: state.index,
+        previousIndex:
+          previousIndex !== undefined ? previousIndex : state.index,
       }
     }
 
