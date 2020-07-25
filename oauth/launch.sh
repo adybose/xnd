7cd1e7b6c9068f022c82470deac3cecd1481e4a5
Anirudha Bose <anirudha.bose@alumni.cern>
2020-07-25 23:57:43 +0530

    Add docker compose file for oauth

--- oauth/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/env bash
-
-docker build -t xnd-oauth:latest .
-docker run -d -e GITHUB_CLIENT_SECRET=$GITHUB_CLIENT_SECRET -p 9000:9000 --rm xnd-oauth:latest
--- oauth/run_prod.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-gunicorn --certfile /etc/letsencrypt/live/xnd.money/fullchain.pem --keyfile /etc/letsencrypt/live/xnd.money/privkey.pem -b 0.0.0.0:9000 app:app
