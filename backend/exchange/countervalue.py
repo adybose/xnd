59c0a046852c31e4ae35834615af664f396d7c61
Anirudha Bose <anirudha.bose@alumni.cern>
2020-07-30 01:31:42 +0530

    Implement a Pythonic countervalue SDK

---
+++ backend/exchange/countervalue.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass
+
+from pycoingecko import CoinGeckoAPI
+
+cg = CoinGeckoAPI()
+
+
+@dataclass
+class Unit:
+    name: str
+    multiplier: int = 1
+    base: "Unit" = None
+
+    def __rmul__(self, amount: float) -> "Amount":
+        return Amount(
+            value=int(amount * self.multiplier),
+            unit=self if self.multiplier == 1 else self.base,
+        )
+
+    def __truediv__(self, other: "Unit") -> float:
+        """
+        This allows the user to query the exchange rate between two units.
+        """
+        # ETH / wei
+        # XRP / drops
+        if self.base == other:
+            return self.multiplier * 1.0
+
+        # wei / ETH
+        # drops / XRP
+        elif self == other.base:
+            return 1.0 / (other / self)
+
+        # wei / wei
+        # XRP / XRP
+        elif self == other:
+            return 1.0
+
+        # ETH / XRP
+        # wei / XRP
+        # ETH / drops
+        # wei / drops
+        elif self in (ETH, wei) and other in (XRP, drops):
+            eth_to_xrp_rate = cg.get_price(ids="ethereum", vs_currencies="xrp")[
+                "ethereum"
+            ]["xrp"]
+
+            in_wei = (1 * self).value
+            in_drops = (1 * other).value
+            return (
+                (in_wei / in_drops)
+                * eth_to_xrp_rate
+                / (ETH.multiplier / XRP.multiplier)
+            )
+
+        # drops / wei
+        # drops / ETH
+        # XRP / wei
+        # XRP / ETH
+        elif self in (XRP, drops) and other in (ETH, wei):
+            return 1.0 / (other / self)
+
+        raise NotImplementedError
+
+
+@dataclass
+class Amount:
+    value: float
+    unit: Unit
+
+    def __repr__(self):
+        return f"{self.value} {self.unit.name}"
+
+    def __mul__(self: "Amount", other: Unit) -> "Amount":
+        ratio = self.unit / other
+        value_in_other = self.value * ratio
+        return Amount(value=value_in_other, unit=other)
+
+
+drops = Unit("drops")
+XRP = Unit("XRP", multiplier=1000000, base=drops)
+wei = Unit("wei")
+ETH = Unit("ETH", multiplier=1000000000000000000, base=wei)
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
---
+++ backend/exchange/test_countervalue.py
@@ -0,0 +1,40 @@
+import pytest
+
+from .countervalue import ETH, XRP, Unit, cg, drops, wei
+
+
+@pytest.fixture(autouse=True)
+def mock_ext_api_call(mocker):
+    mocker.patch.object(cg, "get_price", return_value={"ethereum": {"xrp": 1349}})
+
+
+@pytest.mark.parametrize(
+    "numerator, denominator, expected",
+    [
+        (ETH, wei, 10 ** 18),
+        (wei, ETH, 10 ** -18),
+        (wei, drops, 1.349e-09),
+        (wei, XRP, 1.349e-15),
+        (XRP, wei, 741289844329132.6),
+        (drops, ETH, 7.412898443291327e-10),
+    ],
+)
+def test_unit_conversion(numerator: Unit, denominator: Unit, expected: float):
+    assert (numerator / denominator) == expected
+
+
+def test_unit_conversion_chain():
+    assert (wei / XRP) * (XRP / ETH) * (ETH / drops) * (drops / wei) == 1.0
+
+
+@pytest.mark.parametrize(
+    "value, currency, target, expected",
+    [
+        (1, ETH, XRP, 1349.0),
+        (5, ETH, XRP, 6745.0),
+        (1, ETH, wei, 1e-18),
+        (2, XRP, wei, 1482579688658265.2),
+    ],
+)
+def test_amount_conversion(value, currency, target, expected):
+    assert value * currency * target == expected
